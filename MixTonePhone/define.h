6dd6b3bbf0fa58e15e133ca6ceb3a376c2783cee
yangjianhui <yjhui0331@163.com>
2018-03-20 18:29:05 +0800

    增加呼人呼出挂断，event callback

--- MixTonePhone/define.h
+++ MixTonePhone/define.h
@@ -21,6 +21,15 @@ using namespace std;
 #define SIP_DOMAIN	"192.168.50.200"
 #define SIP_USER	"5011"
 #define SIP_PASSWD	"5011"
+#define SIP_PORT    5060
+
+
+typedef struct tagMT_INCOMINGCALL
+{
+	pjsua_acc_id acc_id;
+	pjsua_call_id call_id;
+	pjsip_rx_data *rdata;
+}MT_INCOMINGCALL, *LPMT_INCOMINGCALL;
 
 
 
